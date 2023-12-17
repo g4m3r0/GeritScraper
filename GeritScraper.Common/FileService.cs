5a406198eae9203dd88df83884de7210aebba34f
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2023-12-17 17:28:59 +0100

    Remove test code and projects

--- GeritScraper.Common/FileService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GeritScraper.Common;
-
-public class FileService
-{
-    public string GetDatabasePath() => Path.Combine(Directory.GetCurrentDirectory(), "Input", "institutionen_gerit.csv");
-    public string GetOutputPath() => Path.Combine(Directory.GetCurrentDirectory(), "Output_new");
-}
